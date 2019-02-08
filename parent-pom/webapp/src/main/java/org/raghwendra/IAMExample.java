d3ea6f3b8a19b60561ae792c14be85da02a5cf95
Raghwendra Singh <raghz1994@gmail.com>
2019-02-08 15:59:21 +0530

    created method to attach user to a group

--- parent-pom/webapp/src/main/java/org/raghwendra/IAMExample.java
+++ parent-pom/webapp/src/main/java/org/raghwendra/IAMExample.java
@@ -2,19 +2,18 @@ package org.raghwendra;
 
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.iam.IamClient;
-import software.amazon.awssdk.services.iam.model.CreateUserRequest;
-import software.amazon.awssdk.services.iam.model.CreateUserResponse;
+import software.amazon.awssdk.services.iam.model.*;
 
 public class IAMExample {
     public IamClient iam; // make this static
     public Region region = Region.AWS_GLOBAL;
-    public IAMExample(AwsBasicCredentials creds) {
+    public IAMExample(String accessKeyId, String secretAccessKey) {
+
         iam = IamClient.builder()
-                .credentialsProvider(StaticCredentialsProvider.create(creds))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKeyId, secretAccessKey)))
                 .region(region).build();
     }
     public String createUser(String userName) {
@@ -22,12 +21,27 @@ public class IAMExample {
         //Region region = Region.AWS_GLOBAL;
         //IamClient iam = IamClient.builder().region(region).build();
 
+
         CreateUserRequest createUserRequest = CreateUserRequest.builder()
-                .userName(userName).build();
+                .userName(userName)
+                .build();
 
         CreateUserResponse response = iam.createUser(createUserRequest);
 
         System.out.println(response.toString());
+
+        return response.toString();
+    }
+
+    public String attachPolicy(String userName, String groupName) {
+        AddUserToGroupRequest addUserToGroupRequest = AddUserToGroupRequest.builder()
+                .groupName(groupName)
+                .userName(userName)
+                .build();
+
+        AttachGroupPolicyRequest.builder().groupName("").policyArn("").build();
+
+        AddUserToGroupResponse response = iam.addUserToGroup(addUserToGroupRequest);
 
         return response.toString();
     }
